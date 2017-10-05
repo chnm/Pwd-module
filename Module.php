b7148c577a2d48215ad9c5ace1355196dd2a29fb
Jim Safley <jimsafley@gmail.com>
2017-10-05 16:42:50 -0400

    Add Images tab to document items

--- Module.php
+++ Module.php
@@ -9,7 +9,13 @@ class Module extends AbstractModule
 {
     public function getConfig()
     {
-        return [];
+        return [
+            'view_manager' => [
+                'template_path_stack' => [
+                    OMEKA_PATH . '/modules/Pwd/view',
+                ],
+            ],
+        ];
     }
 
     public function attachListeners(SharedEventManagerInterface $sharedEventManager)
@@ -18,12 +24,13 @@ class Module extends AbstractModule
             'Omeka\Controller\Admin\Item',
             'view.show.section_nav',
             function (Event $event) {
-                $item = $event->getTarget()->item;
-                if (!$item) {
+                $view = $event->getTarget();
+                $item = $view->item;
+                if (!$this->isClass('pwd:Document', $item)) {
                     return;
                 }
                 $sectionNav = $event->getParam('section_nav');
-                $sectionNav['pwd-item-linked'] = 'Linked resources (PWD)';
+                $sectionNav['pwd-document-images'] = 'Images';
                 $event->setParam('section_nav', $sectionNav);
             }
         );
@@ -31,8 +38,42 @@ class Module extends AbstractModule
             'Omeka\Controller\Admin\Item',
             'view.show.after',
             function (Event $event) {
-                echo '<div id="pwd-item-linked" class="section"><p>Linked resources (PWD)</p></div>';
+                $view = $event->getTarget();
+                $item = $view->item;
+                if (!$this->isClass('pwd:Document', $item)) {
+                    return;
+                }
+                echo $view->partial('pwd/document-images', [
+                    'documentImages' => $this->getDocumentImages($item),
+                ]);
             }
         );
+    }
+
+    protected function isClass($className, $item)
+    {
+        $class = $item->resourceClass();
+        if (!$class) {
+            return false;
+        }
+        if ($className !== $class->term()) {
+            return false;
+        }
+        return true;
+    }
+
+    protected function getDocumentImages($item)
+    {
+        $conn = $this->getServiceLocator()->get('Omeka\Connection');
+
+        $linkedResources = [];
+        $tables = ['pwd_document_collection', 'pwd_document_microfilm', 'pwd_document_publication'];
+        foreach ($tables as $table) {
+            $sql = "SELECT * FROM $table WHERE document_id = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([$item->id()]);
+            $linkedResources[$table] = $stmt->fetchAll();
+        }
+        return $linkedResources;
     }
 }

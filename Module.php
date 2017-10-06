41235c1b103e69820f5e5afcbb1c112fbb086e77
Jim Safley <jimsafley@gmail.com>
2017-10-06 11:29:13 -0400

    Add image documents tab

--- Module.php
+++ Module.php
@@ -26,12 +26,16 @@ class Module extends AbstractModule
             function (Event $event) {
                 $view = $event->getTarget();
                 $item = $view->item;
-                if (!$this->isClass('pwd:Document', $item)) {
-                    return;
+                if ($this->isClass('pwd:Document', $item)) {
+                    $sectionNav = $event->getParam('section_nav');
+                    $sectionNav['pwd-document-images'] = 'Images';
+                    $event->setParam('section_nav', $sectionNav);
                 }
-                $sectionNav = $event->getParam('section_nav');
-                $sectionNav['pwd-document-images'] = 'Images';
-                $event->setParam('section_nav', $sectionNav);
+                if ($this->isClass('pwd:Image', $item)) {
+                    $sectionNav = $event->getParam('section_nav');
+                    $sectionNav['pwd-image-documents'] = 'Documents';
+                    $event->setParam('section_nav', $sectionNav);
+                }
             }
         );
         $sharedEventManager->attach(
@@ -40,12 +44,16 @@ class Module extends AbstractModule
             function (Event $event) {
                 $view = $event->getTarget();
                 $item = $view->item;
-                if (!$this->isClass('pwd:Document', $item)) {
-                    return;
+                if ($this->isClass('pwd:Document', $item)) {
+                    echo $view->partial('pwd/document-images', [
+                        'documentImages' => $this->getDocumentImages($item),
+                    ]);
                 }
-                echo $view->partial('pwd/document-images', [
-                    'documentImages' => $this->getDocumentImages($item),
-                ]);
+                if ($this->isClass('pwd:Image', $item)) {
+                    echo $view->partial('pwd/image-documents', [
+                        'imageDocuments' => $this->getImageDocuments($item),
+                    ]);
+                }
             }
         );
     }
@@ -66,14 +74,29 @@ class Module extends AbstractModule
     {
         $conn = $this->getServiceLocator()->get('Omeka\Connection');
 
-        $linkedResources = [];
+        $documentImages = [];
         $tables = ['pwd_document_collection', 'pwd_document_microfilm', 'pwd_document_publication'];
         foreach ($tables as $table) {
             $sql = "SELECT * FROM $table WHERE document_id = ?";
             $stmt = $conn->prepare($sql);
             $stmt->execute([$item->id()]);
-            $linkedResources[$table] = $stmt->fetchAll();
+            $documentImages[$table] = $stmt->fetchAll();
         }
-        return $linkedResources;
+        return $documentImages;
+    }
+
+    protected function getImageDocuments($item)
+    {
+        $conn = $this->getServiceLocator()->get('Omeka\Connection');
+
+        $imageDocuments = [];
+        $tables = ['pwd_document_collection', 'pwd_document_microfilm', 'pwd_document_publication'];
+        foreach ($tables as $table) {
+            $sql = "SELECT * FROM $table WHERE image_id = ?";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([$item->id()]);
+            $imageDocuments[$table] = $stmt->fetchAll();
+        }
+        return $imageDocuments;
     }
 }

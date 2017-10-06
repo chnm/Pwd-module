e7708d77ea3794075917f6490d2b38ed6fc499dd
Jim Safley <jimsafley@gmail.com>
2017-10-06 17:33:07 -0400

    More viewer dev

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,5 +8,15 @@ class IndexController extends AbstractActionController
 {
     public function viewerAction()
     {
+        $response = $this->api()->read('items', $this->params('image-id'));
+        $image = $response->getContent();
+        if ('pwd:Image' !== $image->resourceClass()->term()) {
+            throw new \Exception('Item must be a pwd:Image to view.');
+        }
+
+        $view = new ViewModel;
+        $item = $response->getContent();
+        $view->setVariable('image', $image);
+        return $view;
     }
 }
